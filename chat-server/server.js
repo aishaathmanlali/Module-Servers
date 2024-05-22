319e493852663395267a4ffb8ae77fe22e938d0c
aishaathmanlali <aishaathmanal@gmail.com>
2024-05-22 20:10:36 +0100

    complete level 1

--- chat-server/server.js
+++ chat-server/server.js
@@ -7,14 +7,15 @@ import { fileURLToPath } from "url";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: "Aisha",
+  text: "Welcome to my chat system!",
 };
 
 //This array is our "data store".
@@ -23,6 +24,45 @@ const messages = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+// To read all the messages
+app.get("/messages", (request, response) => {
+  res.send(messages);
+});
+
+//To read one specific message  by Id
+app.get("/messages/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const chosenMessage = messages.find((message) => message.id === id);
+  if (chosenMessage) {
+    response.send(chosenMessage);
+  } else {
+    response.status(404).send({ error: "Message not found" });
+  }
+});
+
+//To create a new message
+app.post("/messages", (request, response) => {
+  const newMessage = {
+    id: messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+  messages.push(newMessage);
+  response.status(201).send(newMessage);
+});
+
+//To delete a message by Id
+app.delete("/messages/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const messageIndex = messages.findIndex((message) => message.id === id);
+  if (messageIndex !== -1) {
+    messages.splice(messageIndex, 1);
+    response.status(204).send();
+  } else {
+    response.status(404).json({ error: "Message not found" });
+  }
 });
 
 app.listen(process.env.PORT, () => {
